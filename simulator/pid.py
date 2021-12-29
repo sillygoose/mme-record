028e0199e5b4b993190dc3f8fb880643669bc1c9
Rick Naro <sillygoose@me.com>
2021-12-29 14:48:57 -0500

    cleanup

--- simulator/pid.py
+++ simulator/pid.py
@@ -1,22 +1,11 @@
 
 class PID:
-    def __init__(self, id, name) -> None:
+    def __init__(self, id: int, name: str) -> None:
         self._id = id
         self._name = name
-        self._enable = False
-
-    def start(self) -> None:
-        print(f"Starting PID {self._name}")
-        self._enable= True
-
-    def stop(self) -> None:
-        print(f"Stopping PID {self._name}")
-        self._enable = False
 
     def name(self) -> str:
         return self._name
 
     def id(self) -> int:
         return self._id
-
-
