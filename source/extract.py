d13c2e8600096950d0c2b8017c7c17837badfd0c
Rick Naro <sillygoose@me.com>
2022-01-31 11:50:34 -0500

    updated readme and improve extract

--- source/extract.py
+++ source/extract.py
@@ -18,16 +18,13 @@ _LOGGER = logging.getLogger('mme')
 class Extract:
     def __init__(self) -> None:
         self._did_manager = DIDManager()
-        #self._codec_manager = CodecManager()
 
     def start(self) -> None:
         dids = self._did_manager._load_dids(file='json/other/did_coverage.json')
         new_dids = []
         for did_record in dids:
             did_id = did_record.get('did_id')
-            length = did_record.get('length')
-            modules = did_record.get('modules')
-            if 'BCM' in modules and length <= 7:
+            if self.filter(did_record):
                 new_dids.append({'did_name': '???', 'did_id': did_id, 'did_id_hex': f"{did_id:04X}", 'codec_id': -1})
 
         extract_dids = {
@@ -38,10 +35,18 @@ class Extract:
             'period': 10,
             'dids': new_dids
         }
-        self._did_manager._save_dids('json/did/bcm_coverage.json', extract_dids)
+        self._did_manager._save_dids('json/other/bcm_coverage.json', extract_dids)
+        _LOGGER.info(f"Extracted {len(new_dids)} DIDs to the output file")
 
     def stop(self) -> None:
         pass
+
+    def filter(self, did) -> bool:
+        length = did.get('length')
+        modules = did.get('modules')
+        if 'BCM' in modules and length <= 7:
+            return True
+        return False
 
 
 def main() -> None:
